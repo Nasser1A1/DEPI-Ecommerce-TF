5e14591282ee395258a7890a407adbb7a2f286d4
Mahmoud Nasser <100250722+Nasjr@users.noreply.github.com>
2025-12-07 20:26:35 +0200

    removed secretes

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = var.aws_region
-  access_key = "AKIAT7YKXQMZRGDHBHGH"
-  secret_key = "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"
+
   # Or Configure AWS CLI or Environment Variables for credentials
 }
 
